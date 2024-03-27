da46c35e0a4483b7dcbc06e8cec116b98aa189e6
Rishikesh Tapkir <89522956+titan21999@users.noreply.github.com>
2024-03-27 18:33:07 +0530

    Add files via upload

--- sgnpp.php
+++ sgnpp.php
@@ -6,25 +6,26 @@ $mname=$_GET['middlename'];
 $surname=$_GET['lastname'];
 $email=$_GET['mailid'];
 $dateofbday=$_GET['bday'];
-$phoeneum=$_GET['phone'];
+$phoeneum=$_GET['phno'];
 $password=$_GET['password'];
 
 echo"$username,$fname,$mname,$surname";
 
-$host = "root";
-$user = "rishitapkir";
-$psw = "arpita";
-$db = "userinfo";
+$host = "localhost";
+$user = "root";
+$psw = "";
+$database = "userinfo";
 
-$conn = mysqli_conect($host,$user,$pw,$db);
+$conn = mysqli_connect($host,$user,$psw,$database);
 
 if (!$conn) {
     die("Error: Unable to connect to the PostgreSQL database.");
 }
-
 elseif($conn==true){
 
-	echo "Connected to the PostgreSQL database successfully!";
+	echo"Connection Successful";
+	$query="INSERT INTO `users`(`usernm`, `firstnamae`, `middlenamae`, `lastnamae`, `maail`, `birthdayy`, `pohnum`, `passkey`) VALUES ('$username','$fname','$mname','$surname','$email','$dateofbday','$phoeneum','$password')";
+	$execute=mysqli_query($conn,$query);
 
 }
 ?>
